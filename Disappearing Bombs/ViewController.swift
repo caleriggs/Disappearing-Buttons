f5b51f0cbb3fdc6fbf8ca9a4d8842fe99fe20e8b
caleriggs <caleriggs@caleriggsMacBookPro.local>
2016-01-28 21:01:21 -0600

    Disappearing Buttons project I made so I could learn how to do Git stuff

--- Disappearing Bombs/ViewController.swift
+++ Disappearing Bombs/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blueCircle: UIImageView!
+    @IBOutlet weak var redCircle: UIImageView!
+    @IBOutlet weak var hideRed: UIButton!
+    @IBOutlet weak var hideBlue: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func hideRed(sender: AnyObject) {
+        redCircle.hidden = true
+    }
+    @IBAction func hideBlue(sender: AnyObject) {
+        blueCircle.hidden = true
+    }
 
 }
 
